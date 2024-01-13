33356c3545fabd78cf1a7a6556aa8fe315a6f005
bborkowsp <pawel.borkowski0704@gmail.com>
2024-01-13 16:22:09 +0100

    progress bar update

--- src/main/resources/static/js/delete-note-modal-window.js
+++ src/main/resources/static/js/delete-note-modal-window.js
@@ -2,12 +2,28 @@ function setNoteId(noteId) {
     document.getElementById('encryptDecryptId').value = noteId;
 }
 
-function setTitle(isEncrypted) {
+function setTitleAndVisibility(isEncrypted) {
+    const titleElement = document.getElementById('encryptDecryptModalTitle');
+    const submitBtnElement = document.getElementById('encryptDecryptSubmitBtn');
+    const progressBarElement = document.getElementById('entropy-progress-bar');
+    const entropyContainerElement = document.getElementById('entropy-container');
+    const progressBarBodyTextElement = document.getElementById('progress-bar-body-text');
+
     if (isEncrypted) {
-        document.getElementById('encryptDecryptModalTitle').innerText = 'Decrypt';
-        document.getElementById('encryptDecryptSubmitBtn').innerText = 'Decrypt';
+        titleElement.innerText = 'Decrypt';
+        submitBtnElement.innerText = 'Decrypt';
+        progressBarBodyTextElement.innerText = '';
+
+        // Hide progress bar and entropy container
+        progressBarElement.style.display = 'none';
+        entropyContainerElement.style.display = 'none';
     } else {
-        document.getElementById('encryptDecryptModalTitle').innerText = 'Encrypt';
-        document.getElementById('encryptDecryptSubmitBtn').innerText = 'Encrypt';
+        titleElement.innerText = 'Encrypt';
+        submitBtnElement.innerText = 'Encrypt';
+        progressBarBodyTextElement.innerText = 'Remember, encrypted note will not be visible to others.';
+
+        // Show progress bar and entropy container
+        progressBarElement.style.display = 'block';  // or 'flex' based on your styling
+        entropyContainerElement.style.display = 'block';  // or 'flex' based on your styling
     }
 }
